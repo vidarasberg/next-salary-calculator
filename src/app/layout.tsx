d83df99f0e05205ba3ae0af554a74bdfba2715bb
Vidar Åsberg <vidar.asberg@pm.me>
2025-01-02 15:57:44 +0100

    put centering in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} flex items-center justify-center
+ antialiased`}
       >
         {children}
       </body>
