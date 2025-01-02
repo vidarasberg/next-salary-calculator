81708885195cb3ba38f2989233ea35e99b667062
Vidar Åsberg <vidar.asberg@pm.me>
2025-01-02 15:58:15 +0100

    add links to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,84 +1,14 @@
-"use client";
-
-import { ChangeEvent, useState } from "react";
-import CalculatorInput from "./components/calculatorInput";
+import Link from "next/link";
 
 export default function Home() {
-  const [hours, setHours] = useState(160);
-  const [hourlyRate, setHourlyRate] = useState(1000);
-  const [grossSalary, setGrossSalary] = useState(55000);
-  const [pension, setPension] = useState(3000);
-
-  const pensionTax = pension * 0.2426;
-
-  const debitering = hours * hourlyRate;
-  const monthlyIncome = debitering * 0.7;
-
-  const employerFee = grossSalary * 0.3142;
-
-  const totalMonthlyCost = grossSalary + pension + pensionTax + employerFee;
-
-  const remaining = monthlyIncome - totalMonthlyCost;
-
-  function handleHoursChanged(event: ChangeEvent<HTMLInputElement>): void {
-    setHours(Number(event.target.value));
-  }
-
-  function handleHourlyRateChanged(event: ChangeEvent<HTMLInputElement>): void {
-    setHourlyRate(Number(event.target.value));
-  }
-
-  function handleGrossSalaryChanged(
-    event: ChangeEvent<HTMLInputElement>
-  ): void {
-    setGrossSalary(Number(event.target.value));
-  }
-
-  function handlePensionChanged(event: ChangeEvent<HTMLInputElement>): void {
-    setPension(Number(event.target.value));
-  }
-
   return (
-    <div className="flex items-center justify-center">
-      <div className="flex flex-col p-4 gap-2">
-        <h1 className="text-2xl">Lönekalkylator</h1>
-        <div className="flex flex-col gap-4">
-          <h2 className="text-xl">Intäkter / Debitering</h2>
-          <CalculatorInput
-            id="hours"
-            label="Debiterade timmar"
-            value={hours}
-            description="h/månad"
-            onChange={handleHoursChanged}
-          />
-          <CalculatorInput
-            id="hourlyRate"
-            label="Timarvode"
-            value={hourlyRate}
-            description="kr/timme"
-            onChange={handleHourlyRateChanged}
-          />
-          <div>Månadsintäkt: {monthlyIncome}</div>
-          <h2 className="text-xl">Löneuttag / Andra utgifter</h2>
-          <CalculatorInput
-            id="grossSalary"
-            label="Bruttolön"
-            value={grossSalary}
-            description="kr/månad"
-            onChange={handleGrossSalaryChanged}
-          />
-          <div>Arbetsgivaravgift: {employerFee}</div>
-          <CalculatorInput
-            id="pension"
-            label="Pension"
-            value={pension}
-            description="kr/månad"
-            onChange={handlePensionChanged}
-          />
-          <div>Skatt för pension: {pensionTax}</div>
-          <div>Total kostnad (löneuttag + pension): {totalMonthlyCost}</div>
-          <div>Kvar i potten varje månad: {remaining}</div>
-        </div>
+    <div>
+      <p>
+        Detta verktyg kan användas för att beräkna hur man vill göra med lön som anställd hos Independtech
+      </p>
+      <div className="flex h-screen items-center justify-evenly">
+        <Link href={"internal-calculator"} className="p-4 border-solid border-4 border-gray-600 rounded">Internal</Link>
+        <Link href={"external-calculator"} className="p-4 border-solid border-4 border-gray-600 rounded">External</Link>
       </div>
     </div>
   );
